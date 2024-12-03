d5f14f96d20642d5d066fb5f55e50e5f3edbd3d4
np03a170120 <czon.herald75@gmail.com>
2024-12-03 20:50:01 +0545

    fix: deployment fix

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,4 @@
-import React from "react";
 import Navbar from "./Navbar";
-import Logo from "../../../public/images/logo.svg";
-import Image from "next/image";
 
 const Header = () => {
   return (
@@ -9,7 +6,6 @@ const Header = () => {
       <div className="sticky top-0 z-10">
         <div className="bg-primary p-2">
           <div className="container mx-auto flex justify-between items-center">
-            {/* <Image className="h-[2rem]" src={Logo} alt="global square It" /> */}
             <p className="text-white font-bold ">GLOBAL SQUARE</p>
           </div>
         </div>

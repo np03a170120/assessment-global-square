8c9fece359c71cb56f556e72fd9a1a9495593f96
np03a170120 <czon.herald75@gmail.com>
2024-12-03 21:00:27 +0545

    fix: deployment fix

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -2,7 +2,7 @@ import { Products } from "@/types/Products";
 import React from "react";
 import Product from "./Product";
 
-const page = async ({ params }: { params: { id: string } }) => {
+const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const resolvedParams = await params;
   const response = await fetch(
     `https://dummyjson.com/products/${resolvedParams.id}`

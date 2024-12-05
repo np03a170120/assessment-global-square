73d2fb199900e6eacf6070b2bbeea229cb84ad52
np03a170120 <czon.herald75@gmail.com>
2024-12-05 12:26:31 +0545

    fix: deployment

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,26 +1,26 @@
 import { Products } from "@/types/Products";
 import Product from "./Product";
 
-export async function generateMetadata({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id } = await params;
-  const response = await fetch(`https://dummyjson.com/products/${id}`);
-  const data: Products = await response.json();
+// export async function generateMetadata({
+//   params,
+// }: {
+//   params: Promise<{ id: string }>;
+// }) {
+//   const { id } = await params;
+//   const response = await fetch(`https://dummyjson.com/products/${id}`);
+//   const data: Products = await response.json();
 
-  return {
-    title: data.title,
-    description: data.description,
-    openGraph: {
-      images: data.thumbnail,
-      title: data.title,
-      description: data.description,
-      url: `https://dummyjson.com/products/${id}`,
-    },
-  };
-}
+//   return {
+//     title: data.title,
+//     description: data.description,
+//     openGraph: {
+//       images: data.thumbnail,
+//       title: data.title,
+//       description: data.description,
+//       url: `https://dummyjson.com/products/${id}`,
+//     },
+//   };
+// }
 
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
